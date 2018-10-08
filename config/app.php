7bf34f0239f3cc9db9ed97a6c8aea3f340fc2f01
VeloceCat <velocecat@gmail.com>
2018-10-08 21:49:23 +0200

    Form

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
